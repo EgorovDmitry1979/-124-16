58b227292bb9800195b2d53bb519b5aff54b3830
EgorovDmitry1979 <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-05-15 22:17:01 +0300

    Задание №3 по курсу "Технологии программирования".
    Реализация алгоритма 10 в программе на языке C#.
    Приложение:
    Алгоритм 10.pdf

---
+++ Алгоритм 10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Алгоритм_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            string t; //элемент массива статической последовательности
+                int Size = 10; //длина массива
+                Console.WriteLine("Введите произвольное количество данных статической последовательности");
+                Console.WriteLine("После завершения ввода нажмите 'q'");
+            do
+            {
+                t = Console.ReadLine();
+                if (t == "q")
+                {
+                    Console.WriteLine("TRUE. (Последовательность помещается в массив)");
+                    Console.WriteLine("Размер статической последовательности: " + n + ", Размер массива: " + Size);
+                    break;
+                }
+                n++;
+            }
+                while (n <= Size);
+            if (n > Size)
+            {
+                Console.WriteLine("FALSE. (Последовательность НЕ помещается в массив)");
+                Console.WriteLine("Размер статической последовательности: >=" + n + ", Размер массива: " + Size);
+            }
+        }
+    }
+}
